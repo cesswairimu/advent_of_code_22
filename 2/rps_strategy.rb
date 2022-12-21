9e4500746c8f402a7645ec0d6422aec5279c2f40
Cess <cessmbuguar@gmail.com>
2022-12-21 20:27:54 +0300

    part2 day 2

--- 2/rps_strategy.rb
+++ 2/rps_strategy.rb
@@ -5,8 +5,15 @@ class RpsStrategy
   H1 = {"A": "rock", "B": "paper", "C": "scissors" }
   H2 = {"X": "rock", "Y": "paper", "Z": "scissors" }
   H3 = {"X": 1, "Y": 2, "Z": 3 }
+
   H5 = {"X": "C", "Z": "B", "Y": "A" }
-  
+  # with the second input how the round needs to end
+  H4 = { "X": "loss", "Y": "draw", "Z": "win"}
+  H6 = {"A": 1, "B": 2, "C": 3 }
+  H7 = {"X": "C", "Z": "B", "Y": "A" }
+  H8 = { "A":  "C", "B": "A", "C": "B"} 
+
+
 
 
   def input_str
@@ -38,12 +45,42 @@ class RpsStrategy
     end
     score
   end
+
+  def preload_score(arr)
+    a = arr[0]
+    b = arr[1]
+    score = 0
+  
+    if  b == "Y"
+      score += H6[a.to_sym]
+      score += 3
+    elsif b == "X"
+      score += H6[(H8[a.to_sym]).to_sym]
+    elsif b=="Z"
+      score += H6[H8.key(a)]
+      score += 6
+    end
+    score
+  end
+
+  def play_part2
+    total = 0
+    str_pairs_in_array.each do |i|
+       total += preload_score(i) 
+    end
+    total
+  end
 end
 
-def preload_score
-end
+
+
 
 p RpsStrategy.new().score_per_round(["A", "Y"])
 p RpsStrategy.new().score_per_round(["B", "X"])
 p RpsStrategy.new().score_per_round(["C", "Z"])
 p RpsStrategy.new().play
+p RpsStrategy.new().preload_score(["A", "Y"])
+p RpsStrategy.new().preload_score(["B", "X"])
+p RpsStrategy.new().preload_score(["C", "Z"])
+p RpsStrategy.new().play_part2
+
